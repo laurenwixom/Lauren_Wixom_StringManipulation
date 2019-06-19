57d9a467a14ba7d6474080e14868f5fee67bf4bc
Lauren Wixom <50384167+laurenwixom@users.noreply.github.com>
2019-06-18 23:19:55 -0500

    Add files via upload

---
+++ StringManipulator/StringManipulator.java
@@ -0,0 +1,58 @@
+public class trimandconcat (String str1, String str2) {
+	String concatString = "";
+
+	concatString = string 1, string 2;
+System.out.println(sentence.trim())
+
+}
+
+
+
+
+
+public class getIndexorNull (int str1, int str2, int str3) {
+
+String coding = "";
+
+int a = coding.indexOf("coding"); 
+int b = coding.indexOf("hello"); 
+int c = coding.indexOf("world"); 
+
+}
+
+
+
+
+public class getIndexorNull (String str1, String str2, String str3, int str1, int str2) {
+	String helloworld = "";
+
+int a = helloworld.indexOf("coding"); 
+int b = helloworld.indexOf("hello"); 
+
+String substring = "";
+
+	subString = string 1, string 2, string 3;
+System.out.println(sentence.trim())
+ 
+
+}
+
+
+
+
+
+
+public class concatSubstring (){
+	String  concat= "";
+
+String string1 = "My name is ";
+String string2 = "Michael";
+String string3 = string1.concat(string2);
+System.out.println(string3);
+
+
+concat = string 1, string 2, string 3, string 4;
+System.out.println(sentence.trim())
+
+
+}
